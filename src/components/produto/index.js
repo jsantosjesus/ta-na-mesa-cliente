437676000786e4748b22a1589daed68fdab5d62f
jadson santos <jadsoonbol@gmail.com>
2024-03-09 07:01:40 -0300

    poupup de produto

--- src/components/produto/index.js
+++ src/components/produto/index.js
@@ -1,18 +1,97 @@
+import { FaChevronLeft } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import imagemVaziaLogo from '../../assets/ta-na-mesa-logomarca.png';
 
-export const Produto = () => {
+export const Produto = ({ produto, handleClose }) => {
+
+    const [quantidade, setQuantidade] = useState(1);
+
+    const [subtotal, setSubtotal] = useState(0);
+
+    const alterarQuantidade = (operador) => {
+        setQuantidade(quantidade + operador);
+    }
+
+    const calcularSubtotal = () => {
+        setSubtotal(produto.preco * quantidade);
+    }
+
+    const [observacoes, setObservacoes] = useState('')
+
+    useEffect(() => {
+        calcularSubtotal();
+        // eslint-disable-next-line
+    }, [quantidade]);
+
+
+
+    const Subtotal = React.memo(({ subtotal }) => {
+        return (
+            <div><b>R$ {subtotal.toFixed(2).replace(".", ",")}</b></div>
+        );
+    });
+
+
+    ;
+
+    const formulandoProduto = () => {
+
+        let p = {
+            id: produto.id,
+            ...(produto.imagem && { imagem: produto.imagem }),
+            nome: produto.nome,
+            preco: produto.preco,
+            categoria: produto.categoria,
+            observacao: observacoes,
+            quantidade: quantidade
+        }
+
+        return p;
+
+
+    };
+
+
+    const adicionandoProdutoCarrinho = () => {
+        let carrinho = JSON.parse(localStorage.getItem('carrinho'));
+        let novoCarrinho = [];
+        let produtoAdicionado = formulandoProduto();
+        let encontrado = false;
+        let textObservacao = document.getElementById("textObservacao")
+
+        if (carrinho) {
+            novoCarrinho = carrinho;
+            for (let i = 0; i < novoCarrinho.length; i++) {
+                if (novoCarrinho[i].id === produtoAdicionado.id && novoCarrinho[i].observacao === produtoAdicionado.observacao) {
+                    novoCarrinho[i].quantidade = novoCarrinho[i].quantidade + produtoAdicionado.quantidade;
+                    encontrado = true;
+                    break;
+                }
+            }
+        }
+        if (!encontrado) {
+            novoCarrinho.push(produtoAdicionado);
+        }
+        localStorage.setItem('carrinho', JSON.stringify(novoCarrinho));
+        setObservacoes("");
+        setQuantidade(1);
+        textObservacao.value = ("");
+        handleClose();
+
+    };
+
 
     return (
+
         <div className='PoupupProduto'>
 
-            {/* <p className='fecharProdutoIndividual'
-            // onClick={handleClose}
-            ><FaChevronLeft /></p>
-            <div className='poupupProdutoContent'>
-                <img className='imagemProdutoIndividual' src={object.imagem} alt='imagem do produto' />
-                <h3 className='titleProdutoIndividual'>{object.nome}</h3>
-                <p className='descricaoProdutoIndividual'>{object.Descricao}</p>
+            <p className='fecharProdutoIndividual' onClick={handleClose}><FaChevronLeft /></p>
+            {produto && <div className='poupupProdutoContent'>
+                <img className='imagemProdutoIndividual' src={!produto.imagem ? imagemVaziaLogo : produto.imagem} alt='imagem do produto' />
+                <h3 className='titleProdutoIndividual'>{produto.nome}</h3>
+                <p className='descricaoProdutoIndividual'>{produto.descricao}</p>
                 <textarea id='textObservacao' onChange={e => setObservacoes(e.target.value)} placeholder='Observações' />
-            </div>
+            </div>}
             <div className='footerProdutoIndividual'>
                 <div className='PrecoProdutoIndividual'>
                     <Subtotal subtotal={subtotal} />
@@ -28,9 +107,7 @@ export const Produto = () => {
                         onClick={adicionandoProdutoCarrinho}
                     >Adicionar ao Carrinho</button></p>
                 </div>
-            </div> */}
-
-
+            </div>
         </div>
     );
 }
--- src/page/cardapio/index.js
+++ src/page/cardapio/index.js
@@ -1,68 +1,131 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DocsContext } from "../../contexts/docsContext";
 import './cardapio.css';
 import MenuBottom from "../../components/menuBottom";
+import { collection, query, where, getDocs, getFirestore } from "firebase/firestore";
+import { FirebaseContext } from "../../contexts/appContext";
+import imagemVaziaLogo from '../../assets/ta-na-mesa-logomarca.png';
+import { Produto } from "../../components/produto";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 function Cardapio() {
     const { estabelecimento, mesa } = useContext(DocsContext);
 
-return (
-    <div className='bodyCardapio'>
-        <MenuBottom />
-        <div className='headCardapio'>
-            <div className='titleCardapio'>
-                <h1>{estabelecimento.nome}</h1>
-            </div>
+    const [loading, setLoading] = useState();
 
-            <div className='pesquisaCardapio'>
-                <input type="text" placeholder="Pesquise aqui" 
-                // value={searchTerm} onChange={e => setSearchTerm(e.target.value)}
-                ></input>
-            </div>
-            {/* <div className='categorias'>
-                <div 
-                // onClick={() => escolherCategoria('todos')}
-                    className={
-                        'todos' === selecionado ? 'categoriaAtivo' : 'categoriaInativo'
-                    }>
-                    Todos
+    const app = useContext(FirebaseContext);
+    const db = getFirestore(app);
+
+    const [categorias, setCategorias] = useState();
+    const [categoriaSelecionada, setCategoriaSelecinada] = useState();
+
+    const [produtos, setProdutos] = useState();
+    const [produtoSelecionado, setProdutoSelecionado] = useState(null);
+
+    async function getCategorias() {
+        const q = query(collection(db, "categoria"), where("estabelecimento_id", "==", mesa.estabelecimento_id));
+        let firebaseCategorias = [];
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            firebaseCategorias.push({ ...doc.data(), id: doc.id });
+
+        });
+        setCategorias(firebaseCategorias);
+        setCategoriaSelecinada(firebaseCategorias[0]);
+    }
+
+    useEffect(() => {
+        if (!categorias) {
+            getCategorias();
+        }
+    });
+
+
+    // useEffect(() => {
+    //     if (categorias) {
+    //         console.log(categoriaSelecionada);
+    //     }
+    // }, [categoriaSelecionada]);
+
+    async function getProdutos() {
+        if (categoriaSelecionada) {
+            setLoading(true);
+            const q = query(collection(db, "produto"), where("categoria_id", "==", categoriaSelecionada.id));
+            let firebaseProdutos = [];
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                firebaseProdutos.push({ ...doc.data(), id: doc.id });
+
+            });
+            setLoading(false);
+            setProdutos(firebaseProdutos);
+        }
+    }
+
+
+    useEffect(() => {
+        getProdutos();
+        // eslint-disable-next-line
+    }, [categoriaSelecionada]);
+
+
+
+    return (
+        <div className='bodyCardapio'>
+            <MenuBottom />
+            <div className='headCardapio'>
+                <div className='titleCardapio'>
+                    <h1>{estabelecimento.nome}</h1>
                 </div>
-                {categorias.map((categorias, index) => (
 
-                    <div onClick={() => escolherCategoria(categorias.nome)}
-                        key={categorias.nome}
-                        className={
-                            categorias.nome === selecionado ? 'categoriaAtivo' : 'categoriaInativo'
-                        }>
-                        {categorias.nome}
-                    </div>
+                <div className='pesquisaCardapio'>
+                    <input type="text" placeholder="Pesquise aqui"
+                    // value={searchTerm} onChange={e => setSearchTerm(e.target.value)}
+                    ></input>
+                </div>
+                <div className='categorias'>
+                    {categorias && categorias.map((categoria, index) => (
 
-                ))}
-            </div> */}
-        </div>
-        <div className='produtosCardapio'>
-            {/* {filteredData.map((object, id) => (
-                <div>
-                    <div className="produtos"
-                        // onClick={() => handleClick(object)}
-                    >
+                        <div
+                            onClick={() => setCategoriaSelecinada(categoria)}
+                            key={index}
+                            className={
+                                categoria === categoriaSelecionada ? 'categoriaAtivo' : 'categoriaInativo'
+                            }
+                        >
+                            {categoria.nome}
+                        </div>
 
-                        <img className='produtoImagem' src={object.imagem} alt='imagem do produto' />
-                        <div>
-                            <h3>{object.nome}</h3>
-                            <p className="produtoDescricao">{object.Descricao}</p>
-                            <p className='produtoPreco'>R$ {object.preco.toFixed(2).replace(".", ",")}</p>
+                    ))}
+                </div>
+            </div>
+            {loading ? <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '70vh' }}>
+                <CircularProgress />
+            </Box> : <div className='produtosCardapio'>
+                {produtos && produtos.map((produto) => (
+                    <div>
+                        <div className="produtos"
+                            onClick={() => setProdutoSelecionado(produto)}
+                            key={produto}
+                        >
+
+                            <img className='produtoImagem' src={produto.imagem ? produto.imagem : imagemVaziaLogo} alt='imagem do produto' />
+                            <div>
+                                <h3>{produto.nome}</h3>
+                                <p className="produtoDescricao">{produto.descricao}</p>
+                                <p className='produtoPreco'>R$ {produto.preco.toFixed(2).replace(".", ",")}</p>
+                            </div>
                         </div>
                     </div>
-                    <Popup object={object} />
-                </div>
 
-            )
+                )
 
-            )
-            } */}
+                )
+                }
+            </div>}
+            {produtoSelecionado && <Produto produto={produtoSelecionado} handleClose={() => setProdutoSelecionado(null)} />}
         </div>
-    </div>
-);
+    );
 
 } export default Cardapio;
